403e29f2fc30e65444206722dbe3f095b37c3daa
Rafael Martinez Montiel <rafael.martinez.montiel@gmail.com>
2024-03-04 23:09:49 -0600

    Update tests for JWT and validate data ownership

--- tests/integration/test_owner_api_gateway.py
+++ tests/integration/test_owner_api_gateway.py
@@ -4,21 +4,35 @@ import boto3
 import pytest
 import requests
 import os
+from data_vault import authorizer
+
 """
 Make sure env variable AWS_SAM_STACK_NAME exists with the name of the stack we are going to test. 
 """
 
+dir()
 
 class TestApiGateway:
 
     @pytest.fixture()
     def api_gateway_url(self):
        return os.environ["VAULT_API_URL"]
+    
+    # fixture to return user 1 authorization
+    @pytest.fixture()
+    def user1_auth(self):
+        return authorizer.create_jwt_from_principal('user1')
+    
+    @pytest.fixture()
+    def user2_auth(self):
+        return authorizer.create_jwt_from_principal('user2')
 
 
-    def test_api_gateway_store(self, api_gateway_url):
+    def test_api_gateway_store(self, api_gateway_url, user1_auth):
         """ Call the API Gateway endpoint and check the response """
-        response = requests.post(api_gateway_url, json={
+        response = requests.post(api_gateway_url, 
+            headers={"Authentication":user1_auth},
+            json={
             "elements": {
                 "givenname":{
                     "value":"my name", "classification":"name"},
@@ -36,14 +50,16 @@ class TestApiGateway:
         validate_token_in_element(contact)
 
 
-    def test_api_gateway_remove(self, api_gateway_url):
+    def test_api_gateway_remove(self, api_gateway_url, user1_auth):
         """ Call the API Gateway endpoint and check the response """
-        data_tokens = self.invoke_store_data(api_gateway_url, "givenname", "givenname", "name")
+        data_tokens = self.invoke_store_data(api_gateway_url, "givenname", "givenname", "name", user1_auth)
 
         data_element = validate_alias_in_data_elements(data_tokens, "givenname")
         token = validate_token_in_element(data_element)
         
-        response = requests.patch(api_gateway_url, json={
+        response = requests.patch(api_gateway_url, 
+            headers={"Authentication":user1_auth},
+            json={
             "elements": {
                 "givenname": token}})
 
@@ -53,12 +69,32 @@ class TestApiGateway:
         assert "success" in data_element, f"Data element: {data_element}"
         assert data_element["success"] == True
 
+    def test_api_gateway_remove_with_different_user(self, api_gateway_url, user1_auth, user2_auth):
+        """ Call the API Gateway endpoint and check the response """
+        data_tokens = self.invoke_store_data(api_gateway_url, "givenname", "givenname", "name", user1_auth)
 
-    def invoke_store_data(self, api_gateway_url, alias, value, classification):
+        data_element = validate_alias_in_data_elements(data_tokens, "givenname")
+        token = validate_token_in_element(data_element)
+
+        response = requests.patch(api_gateway_url,
+            headers={"Authentication":user2_auth},
+            json={
+            "elements": {
+                "givenname": token}})
+
+        assert response.status_code == 403, f"Response: {response.content}"
+        body = response.json()
+        assert "error" in body, f"Body: {body}"
+        assert body["error"] == "Forbidden"
+
+
+    def invoke_store_data(self, api_gateway_url, alias, value, classification, authorization):
         data_elements = dict()
         data_elements[alias] = {"value":value, "classification":classification}
-        response = requests.post(api_gateway_url, json={
-            "elements": data_elements})
+        response = requests.post(api_gateway_url,
+            headers={"Authentication": authorization}, 
+            json={
+                "elements": data_elements})
 
         assert response.status_code == 200
 
