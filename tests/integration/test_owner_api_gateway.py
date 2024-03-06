61ba344aff0288d916754fe1184dadeb404ed6ff
Rafael Martinez Montiel <rafael.martinez.montiel@gmail.com>
2024-03-06 15:22:01 -0600

    Test Case. Check validation of ownership of data.
    Avoid removal of not owned data

--- tests/integration/test_owner_api_gateway.py
+++ tests/integration/test_owner_api_gateway.py
@@ -4,7 +4,7 @@ import boto3
 import pytest
 import requests
 import os
-from data_vault import authorizer
+from authorizer import authorizer
 
 """
 Make sure env variable AWS_SAM_STACK_NAME exists with the name of the stack we are going to test. 
@@ -21,17 +21,17 @@ class TestApiGateway:
     # fixture to return user 1 authorization
     @pytest.fixture()
     def user1_auth(self):
-        return authorizer.create_jwt_from_principal('user1')
+        return authorizer.create_jwt_from_principal('aid1')
     
     @pytest.fixture()
     def user2_auth(self):
-        return authorizer.create_jwt_from_principal('user2')
+        return authorizer.create_jwt_from_principal('aid2')
 
 
     def test_api_gateway_store(self, api_gateway_url, user1_auth):
         """ Call the API Gateway endpoint and check the response """
         response = requests.post(api_gateway_url, 
-            headers={"Authentication":user1_auth},
+            headers={"Authorization": user1_auth, "Content-Type":"application/json"}, 
             json={
             "elements": {
                 "givenname":{
@@ -41,7 +41,7 @@ class TestApiGateway:
                 "phone1":{
                         "value":"+521234567890", "classification":"phone"}}})
 
-        assert response.status_code == 200
+        assert response.status_code == 200, f"Response: {response.content}"
         body = response.json()
 
         given_name = validate_alias_in_body_data_elements(body, "elements", "givenname")
@@ -57,11 +57,7 @@ class TestApiGateway:
         data_element = validate_alias_in_data_elements(data_tokens, "givenname")
         token = validate_token_in_element(data_element)
         
-        response = requests.patch(api_gateway_url, 
-            headers={"Authentication":user1_auth},
-            json={
-            "elements": {
-                "givenname": token}})
+        response = self.invoke_remove_data(api_gateway_url, "givenname", token, user1_auth)
 
         assert response.status_code == 200, f"Response: {response.content}"
         body = response.json()
@@ -76,11 +72,7 @@ class TestApiGateway:
         data_element = validate_alias_in_data_elements(data_tokens, "givenname")
         token = validate_token_in_element(data_element)
 
-        response = requests.patch(api_gateway_url,
-            headers={"Authentication":user2_auth},
-            json={
-            "elements": {
-                "givenname": token}})
+        response = self.invoke_remove_data(api_gateway_url, "givenname", token, user2_auth)
 
         assert response.status_code == 403, f"Response: {response.content}"
         body = response.json()
@@ -92,16 +84,23 @@ class TestApiGateway:
         data_elements = dict()
         data_elements[alias] = {"value":value, "classification":classification}
         response = requests.post(api_gateway_url,
-            headers={"Authentication": authorization}, 
+            headers={"Authorization": authorization, "Content-Type":"application/json"}, 
             json={
                 "elements": data_elements})
 
-        assert response.status_code == 200
+        assert response.status_code == 200, f"Response: {response.content}"
 
         body = response.json()
         validate_alias_in_body_data_elements(body,"elements", alias)
         return body["elements"]
-    
+
+    def invoke_remove_data(self, api_gateway_url, alias, token, authorization):
+        response = requests.patch(f"{api_gateway_url}",
+            headers={"Authorization":authorization, "Content-Type":"application/json"},
+            json={
+            "elements": {
+                f"{alias}": token}})
+        return response
 def validate_alias_in_body_data_elements(body, container, alias):
     assert container in body
     data_elements = body[container]
